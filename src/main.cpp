97a8609f6ad7a3cac3d7e0ef369e0de78a5d1e72
Zachary Vogelhut <zachary.vogelhut@systemswithintelligence.com>
2021-03-16 14:51:01 -0400

    Use ESP8266HTTPClient and WiFiClientSecure to make HTTPS request if necessary, add Serial debug

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,66 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 
-#define BUTTON D4
-#define LED D0
+#define BUTTON D1
+#define LED D4
 
 #define WIFI_SSID ""
 #define WIFI_PASS ""
 char server[] = "www.google.com";
-WiFiClient client;
+WiFiClientSecure client;
 
-void setup() 
+int buttonState = LOW;
+
+void setup()
 {
   pinMode(BUTTON, INPUT);
   pinMode(LED, OUTPUT);
 
+  // Serial setup
+  Serial.begin(115200);
+  while (!Serial); // Wait for Serial to connect
+
   // WiFi setup
   WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED)
   {
+    Serial.print(".");
     delay(500);
   }
+  Serial.println("Connected");
+
+  client.setInsecure();
 }
 
-void loop() 
+void loop()
 {
-  if (digitalRead(BUTTON) == HIGH)
+  int buttonRead = digitalRead(BUTTON);
+
+  if (buttonRead == HIGH)
   {
-    if (client.connect(server, 80)) 
+    digitalWrite(LED, LOW);
+  } else {
+    digitalWrite(LED, HIGH);
+  }
+
+  if (buttonRead != buttonState)
+  {
+    buttonState = buttonRead;
+
+    // button pressed
+    if (buttonState == LOW)
     {
-      // Make HTTP request:
-      client.println("GET /search?q=arduino HTTP/1.0");
-      client.println();
-      // debounce
-      delay(2000);
+      Serial.println("Button has been pressed, making request");
+      HTTPClient request;
+      if (request.begin(client, server))
+      {
+        // Make HTTP request, we don't care about the response
+        request.GET();
+        // debounce
+        delay(5000);
+      }
     }
   }
 }
